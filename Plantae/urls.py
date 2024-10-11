55347fb3af85df700f68cad57e17faa8d53dd2f4
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-10-11 20:56:04 -0300

    coloquei o calendário como aba inicial

--- Plantae/urls.py
+++ Plantae/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import DashboardView
+from .views import CalendarView, DashboardView
 
 
 urlpatterns = [
-    path("", DashboardView.as_view(), name="dashboard"),
+    path("", CalendarView.as_view(), name="calendar"),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", include("site_cc.urls")),
--- Plantae/views.py
+++ Plantae/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
 from site_cc.models import Event
+from site_cc.forms import EventForm
 
 
-class DashboardView(LoginRequiredMixin, View):
+class DashboardView(View):
     login_url = "accounts:signin"
     template_name = "site_cc/dashboard.html"
 
@@ -19,3 +19,52 @@ class DashboardView(LoginRequiredMixin, View):
             "latest_events": latest_events,
         }
         return render(request, self.template_name, context)
+
+class CalendarView(LoginRequiredMixin, View):
+    login_url = "accounts:signin"
+
+    def get(self, request, *args, **kwargs):
+        event_id = request.GET.get('event_id')
+        if request.method == "POST":
+            event_id = request.POST.get('event_id')
+            if event_id:
+                event = get_object_or_404(Event, pk=event_id, user=request.user)
+                form = EventForm(request.POST, instance=event)
+            else:
+                form = EventForm(request.POST)
+
+            if form.is_valid():
+                new_event = form.save(commit=False)
+                new_event.user = request.user
+                new_event.save()
+                return redirect("site_cc:calendar")
+
+        else:
+            form = EventForm()
+            if event_id:
+                event = get_object_or_404(Event, id=event_id, user=request.user)
+                form = EventForm(instance=event)
+
+        events = Event.objects.get_all_events(user=request.user)
+        events_month = Event.objects.get_running_events(user=request.user)
+        event_list = []
+
+        for event in events:
+            event_list.append({
+                "id": event.id,
+                "title": event.title,
+                "type": event.type,
+                "cultura": event.cultura,
+                "local": event.local,
+                "start": event.start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                "end": event.end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                "description": event.description,
+                "duration_readable": event.duration_readable,
+            })
+
+        context = {
+            "form": form,
+            "events": event_list,
+            "events_month": events_month
+        }
+        return render(request, "site_cc/calendar.html", context)
