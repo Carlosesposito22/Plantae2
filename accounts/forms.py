6d83b5125a0c57946071a9a7bef1525240df934e
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-15 21:03:44 -0300

    adição de placeholder para o cadastro e login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,39 +1,66 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.password_validation import validate_password
-
 from accounts.models import User
 
-
 class SignInForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite seu email",
+            }
+        )
+    )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control"})
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha",
+            }
+        )
     )
 
 
 class SignUpForm(forms.ModelForm):
     password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        label="Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Crie uma senha",
+            }
+        ),
         validators=[validate_password],
     )
     password2 = forms.CharField(
-        label="Confirm Password",
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        label="Confirmar Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirme sua senha",
+            }
+        ),
         validators=[validate_password],
     )
 
     class Meta:
         model = User
         fields = ["email"]
-        widgets = {"email": forms.EmailInput(attrs={"class": "form-control"})}
+        widgets = {
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Digite seu email",
+                }
+            )
+        }
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise ValidationError("Password didn't match!")
+            raise ValidationError("As senhas não coincidem!")
         return password2
 
     def save(self, commit=True):
