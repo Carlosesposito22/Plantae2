a80c75e6fab5562feda9fc0bc262b7f507b4912c
João Miccuci <joaomicucci3@gmail.com>
2024-11-25 22:16:45 -0300

    comando de deletar os usuarios para os testes

--- Plantae/settings.py
+++ Plantae/settings.py
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "site_cc:mainpage"
+AUTH_USER_MODEL = 'accounts.User'
---
+++ site_cc/management/commands/deleteusuarios.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Delete all users'
+
+    def handle(self, *args, **options):
+        User = get_user_model()  
+        User.objects.all().delete() 
+        self.stdout.write(self.style.SUCCESS('Todos os usuários foram deletados com sucesso.'))
