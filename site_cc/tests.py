336b31dd18f7d87935cbf371f4d62a6c5fc1d0c6
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-26 17:10:23 -0300

    Teste automatizado de Editar/Excluir culturas

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -350,3 +350,337 @@ class SugerirColheitaTest(LiveServerTestCase):
             btn_proxMes.click()
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(5)
+
+
+class EditarCulturaTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        #chrome_options.add_argument("--headless")
+        cls.driver = webdriver.Chrome(options=chrome_options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def tearDown(self):
+        subprocess.run(['python', 'manage.py', 'deleteusuarios'], check=True)
+        super().tearDown()
+
+    def testeEditarCultura(self):
+        driver = self.driver
+
+        driver.get("http://localhost:8000/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_junta_se")))
+        btn_junta_se = driver.find_element(By.NAME, "btn_junta_se")
+        time.sleep(1)
+        btn_junta_se.click()
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signup/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email-usuario")))
+        email_registro = driver.find_element(By.ID, "email-usuario")
+        senha1 = driver.find_element(By.NAME, "password1")
+        senha2 = driver.find_element(By.NAME, "password2")
+        btn_registrar = driver.find_element(By.NAME, "btn_registar")
+
+        email_registro.send_keys("userteste@gmail.com")
+        senha1.send_keys("@MinhasenhaForte1234")
+        senha2.send_keys("@MinhasenhaForte1234")
+        time.sleep(2)
+        btn_registrar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signin/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_email")))
+        email_login = driver.find_element(By.ID, "id_email")
+        senhalogin = driver.find_element(By.ID, "id_password")
+        btn_logar = driver.find_element(By.NAME, "btn_logar")
+
+        email_login.send_keys("userteste@gmail.com")
+        senhalogin.send_keys("@MinhasenhaForte1234")
+        time.sleep(1)
+        btn_logar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_addEvento")))
+        btn_addEvento = driver.find_element(By.NAME, "btn_addEvento")
+        btn_addEvento.click()
+
+        time.sleep(3)
+
+        nomeEvento_cultura = driver.find_element(By.ID, "id_title")
+        tipo_cultura = Select(driver.find_element(By.ID, "id_type"))
+        cultura_cultura = Select(driver.find_element(By.ID, "id_cultura"))
+        local_cultura = driver.find_element(By.ID, "id_local")
+        descricao_cultura = driver.find_element(By.ID, "id_description")
+        dataInicio_cultura = driver.find_element(By.ID, "id_start_time")
+        dataFim_cultura = driver.find_element(By.ID, "id_end_time")
+        salvar_btn = driver.find_element(By.CSS_SELECTOR, ".save-btn")
+
+        nomeEvento_cultura.send_keys("Teste para editar cultura")
+        time.sleep(1)
+        tipo_cultura.select_by_visible_text("Preparo")
+        time.sleep(1)
+        cultura_cultura.select_by_visible_text("Rúcula")
+        time.sleep(1)
+        local_cultura.send_keys("Lote 222 - linha 10")
+        time.sleep(1)
+        descricao_cultura.send_keys("Descrição teste para o plantio de Rúcula ")
+        time.sleep(1)
+        dataInicio_cultura.send_keys("25/11/2024")
+        dataInicio_cultura.send_keys(Keys.TAB)
+        dataInicio_cultura.send_keys("10:00")
+        time.sleep(1)
+        dataFim_cultura.send_keys("27/11/2024")
+        dataFim_cultura.send_keys(Keys.TAB)
+        dataFim_cultura.send_keys("12:00")
+        time.sleep(1)
+
+        salvar_btn.click()
+        time.sleep(6)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_gerenciarCultura")))
+        btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura")
+        btn_gerenciarCultura.click()
+        time.sleep(3)
+        assert "Teste para editar cultura" in driver.page_source
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fc-daygrid-event")))
+        cultura_excluida = driver.find_element(By.CSS_SELECTOR, ".fc-daygrid-event")
+        cultura_excluida.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "edit-event-button")))
+        btn_editarCultura = driver.find_element(By.ID, "edit-event-button")
+        btn_editarCultura.click()
+
+        nomeEvento_culturaEditar = driver.find_element(By.ID, "id_title")
+        salvar_btn = driver.find_element(By.CSS_SELECTOR, ".save-btn")
+
+        time.sleep(2)
+        nomeEvento_culturaEditar.clear()
+        time.sleep(1)
+        nomeEvento_culturaEditar.send_keys("Novo nome para o evento")
+        time.sleep(2)
+
+        salvar_btn.click()
+        time.sleep(5)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_gerenciarCultura")))
+        btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura")
+        btn_gerenciarCultura.click()
+        time.sleep(3)
+        assert "Novo nome para o evento" in driver.page_source
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        time.sleep(3)
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fc-daygrid-event")))
+        cultura_excluida = driver.find_element(By.CSS_SELECTOR, ".fc-daygrid-event")
+        cultura_excluida.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "edit-event-button")))
+        btn_editarCultura = driver.find_element(By.ID, "edit-event-button")
+        btn_editarCultura.click()
+
+        cultura_cultura = Select(driver.find_element(By.ID, "id_cultura"))
+        local_cultura = driver.find_element(By.ID, "id_local")
+        dataInicio_cultura = driver.find_element(By.ID, "id_start_time")
+        dataFim_cultura = driver.find_element(By.ID, "id_end_time")
+        salvar_btn = driver.find_element(By.CSS_SELECTOR, ".save-btn")
+
+        time.sleep(2)
+        cultura_cultura.select_by_visible_text("Tomate")
+        time.sleep(2)
+        local_cultura.clear()
+        local_cultura.send_keys("Novo lugar")
+        time.sleep(2)
+        dataInicio_cultura.clear()
+        dataFim_cultura.clear()
+        dataInicio_cultura.send_keys("29/11/2024")
+        dataInicio_cultura.send_keys(Keys.TAB)
+        dataInicio_cultura.send_keys("10:00")
+        time.sleep(1)
+        dataFim_cultura.send_keys("30/11/2024")
+        dataFim_cultura.send_keys(Keys.TAB)
+        dataFim_cultura.send_keys("12:00")
+        time.sleep(1)
+
+        salvar_btn.click()
+        time.sleep(6)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_gerenciarCultura")))
+        btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura")
+        btn_gerenciarCultura.click()
+        time.sleep(3)
+        assert "Novo nome para o evento" in driver.page_source
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(4)
+
+
+class ExcluirCulturaTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        #chrome_options.add_argument("--headless")
+        cls.driver = webdriver.Chrome(options=chrome_options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def tearDown(self):
+        subprocess.run(['python', 'manage.py', 'deleteusuarios'], check=True)
+        super().tearDown()
+
+    def testeExcluirCultura(self):
+        driver = self.driver
+
+        driver.get("http://localhost:8000/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_junta_se")))
+        btn_junta_se = driver.find_element(By.NAME, "btn_junta_se")
+        time.sleep(1)
+        btn_junta_se.click()
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signup/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email-usuario")))
+        email_registro = driver.find_element(By.ID, "email-usuario")
+        senha1 = driver.find_element(By.NAME, "password1")
+        senha2 = driver.find_element(By.NAME, "password2")
+        btn_registrar = driver.find_element(By.NAME, "btn_registar")
+
+        email_registro.send_keys("userteste@gmail.com")
+        senha1.send_keys("@MinhasenhaForte1234")
+        senha2.send_keys("@MinhasenhaForte1234")
+        time.sleep(2)
+        btn_registrar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signin/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_email")))
+        email_login = driver.find_element(By.ID, "id_email")
+        senhalogin = driver.find_element(By.ID, "id_password")
+        btn_logar = driver.find_element(By.NAME, "btn_logar")
+
+        email_login.send_keys("userteste@gmail.com")
+        senhalogin.send_keys("@MinhasenhaForte1234")
+        time.sleep(1)
+        btn_logar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_addEvento")))
+        btn_addEvento = driver.find_element(By.NAME, "btn_addEvento")
+        btn_addEvento.click()
+
+        time.sleep(3)
+
+        nomeEvento_cultura = driver.find_element(By.ID, "id_title")
+        tipo_cultura = Select(driver.find_element(By.ID, "id_type"))
+        cultura_cultura = Select(driver.find_element(By.ID, "id_cultura"))
+        local_cultura = driver.find_element(By.ID, "id_local")
+        descricao_cultura = driver.find_element(By.ID, "id_description")
+        dataInicio_cultura = driver.find_element(By.ID, "id_start_time")
+        dataFim_cultura = driver.find_element(By.ID, "id_end_time")
+        salvar_btn = driver.find_element(By.CSS_SELECTOR, ".save-btn")
+
+        nomeEvento_cultura.send_keys("Teste para excluir cultura")
+        time.sleep(1)
+        tipo_cultura.select_by_visible_text("Outros")
+        time.sleep(1)
+        cultura_cultura.select_by_visible_text("Batata")
+        time.sleep(1)
+        local_cultura.send_keys("Lote 230 - linha 90")
+        time.sleep(1)
+        descricao_cultura.send_keys("Descrição teste para o plantio de Batata")
+        time.sleep(1)
+        dataInicio_cultura.send_keys("22/11/2024")
+        dataInicio_cultura.send_keys(Keys.TAB)
+        dataInicio_cultura.send_keys("10:00")
+        time.sleep(1)
+        dataFim_cultura.send_keys("24/11/2024")
+        dataFim_cultura.send_keys(Keys.TAB)
+        dataFim_cultura.send_keys("12:00")
+        time.sleep(1)
+
+        salvar_btn.click()
+        time.sleep(6)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_gerenciarCultura")))
+        btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura")
+        btn_gerenciarCultura.click()
+        time.sleep(3)
+        assert "Teste para excluir cultura" in driver.page_source
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fc-daygrid-event")))
+        cultura_excluida = driver.find_element(By.CSS_SELECTOR, ".fc-daygrid-event")
+        cultura_excluida.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "delete-event-button")))
+        btn_excluirCultura = driver.find_element(By.ID, "delete-event-button")
+        btn_excluirCultura.click()
+
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "confirmDeleteButton")))
+        btn_confirmarExcluir = driver.find_element(By.ID, "confirmDeleteButton")
+        btn_confirmarExcluir.click()
+        time.sleep(6)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_gerenciarCultura1")))
+        btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura1")
+        btn_gerenciarCultura.click()
+        time.sleep(3)
