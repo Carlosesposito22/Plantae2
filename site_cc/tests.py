e5d621cecc2ec7e24c363644362c0f7df1fc6cc7
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-26 18:26:17 -0300

    Teste automatizado de Clima e Fases da Lua

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -684,3 +684,170 @@ class ExcluirCulturaTest(LiveServerTestCase):
         btn_gerenciarCultura = driver.find_element(By.NAME, "btn_gerenciarCultura1")
         btn_gerenciarCultura.click()
         time.sleep(3)
+
+
+class ExibirClimaETempoTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        #chrome_options.add_argument("--headless")
+        cls.driver = webdriver.Chrome(options=chrome_options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def tearDown(self):
+        subprocess.run(['python', 'manage.py', 'deleteusuarios'], check=True)
+        super().tearDown()
+
+    def testeMostrarTempo(self):
+        driver = self.driver
+
+        driver.get("http://localhost:8000/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_junta_se")))
+        btn_junta_se = driver.find_element(By.NAME, "btn_junta_se")
+        time.sleep(1)
+        btn_junta_se.click()
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signup/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email-usuario")))
+        email_registro = driver.find_element(By.ID, "email-usuario")
+        senha1 = driver.find_element(By.NAME, "password1")
+        senha2 = driver.find_element(By.NAME, "password2")
+        btn_registrar = driver.find_element(By.NAME, "btn_registar")
+
+        email_registro.send_keys("userteste@gmail.com")
+        senha1.send_keys("@MinhasenhaForte1234")
+        senha2.send_keys("@MinhasenhaForte1234")
+        time.sleep(2)
+        btn_registrar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signin/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_email")))
+        email_login = driver.find_element(By.ID, "id_email")
+        senhalogin = driver.find_element(By.ID, "id_password")
+        btn_logar = driver.find_element(By.NAME, "btn_logar")
+
+        email_login.send_keys("userteste@gmail.com")
+        senhalogin.send_keys("@MinhasenhaForte1234")
+        time.sleep(1)
+        btn_logar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "modeTempo")))
+        btn_Clima = driver.find_element(By.ID, "modeTempo")
+        btn_Clima.click()
+
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".wi.wi-day-sunny.day-icon")))
+        icone_climaTeste = driver.find_element(By.CSS_SELECTOR, ".wi.wi-day-sunny.day-icon")
+        icone_climaTeste.click()
+
+        time.sleep(4)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fecharmodalClima")))
+        fechar_modal = driver.find_element(By.ID, "fecharmodalClima")
+        fechar_modal.click()
+
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".wi.wi-day-sprinkle.day-icon")))
+        icone_climaTeste = driver.find_element(By.CSS_SELECTOR, ".wi.wi-day-sprinkle.day-icon")
+        icone_climaTeste.click()
+
+        time.sleep(4)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fecharmodalClima")))
+        fechar_modal = driver.find_element(By.ID, "fecharmodalClima")
+        fechar_modal.click()
+
+    def testeMostrarFasesDaLua(self):
+        driver = self.driver
+
+        driver.get("http://localhost:8000/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_junta_se")))
+        btn_junta_se = driver.find_element(By.NAME, "btn_junta_se")
+        time.sleep(1)
+        btn_junta_se.click()
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signup/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email-usuario")))
+        email_registro = driver.find_element(By.ID, "email-usuario")
+        senha1 = driver.find_element(By.NAME, "password1")
+        senha2 = driver.find_element(By.NAME, "password2")
+        btn_registrar = driver.find_element(By.NAME, "btn_registar")
+
+        email_registro.send_keys("userteste@gmail.com")
+        senha1.send_keys("@MinhasenhaForte1234")
+        senha2.send_keys("@MinhasenhaForte1234")
+        time.sleep(2)
+        btn_registrar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        driver.get("http://localhost:8000/accounts/signin/")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_email")))
+        email_login = driver.find_element(By.ID, "id_email")
+        senhalogin = driver.find_element(By.ID, "id_password")
+        btn_logar = driver.find_element(By.NAME, "btn_logar")
+
+        email_login.send_keys("userteste@gmail.com")
+        senhalogin.send_keys("@MinhasenhaForte1234")
+        time.sleep(1)
+        btn_logar.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "btn_calendario")))
+        btn_calendar = driver.find_element(By.NAME, "btn_calendario")
+        btn_calendar.click()
+
+        time.sleep(3)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "modeFasesLua")))
+        btn_FasesLua = driver.find_element(By.ID, "modeFasesLua")
+        btn_FasesLua.click()
+
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".wi.wi-moon-new.moon-icon")))
+        icone_LuaTeste = driver.find_element(By.CSS_SELECTOR, ".wi.wi-moon-new.moon-icon")
+        icone_LuaTeste.click()
+
+        time.sleep(4)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fecharmodalClima1")))
+        fechar_modal = driver.find_element(By.ID, "fecharmodalClima1")
+        fechar_modal.click()
+
+        time.sleep(2)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".wi.wi-moon-waxing-crescent-3.moon-icon")))
+        icone_LuaTeste = driver.find_element(By.CSS_SELECTOR, ".wi.wi-moon-waxing-crescent-3.moon-icon")
+        icone_LuaTeste.click()
+
+        time.sleep(4)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fecharmodalClima1")))
+        fechar_modal = driver.find_element(By.ID, "fecharmodalClima1")
+        fechar_modal.click()
+
+        time.sleep(2)
