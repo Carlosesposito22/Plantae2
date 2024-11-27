e88a638a610be75b0a7a59044109c165a4bc0610
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-27 16:21:49 -0300

    sem ver

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -35,7 +35,7 @@ class AdicionarCulturaTest(LiveServerTestCase):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1280,720")
         cls.driver = webdriver.Chrome(options=chrome_options)
 
