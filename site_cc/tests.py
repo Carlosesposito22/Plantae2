fbd1dc2300587f0c4144b62aecd3be4badc04886
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-27 16:36:01 -0300

    agora vaiii

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -31,12 +31,12 @@ class AdicionarCulturaTest(LiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--window-size=1280,720")
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
         cls.driver = webdriver.Chrome(options=chrome_options)
 
     @classmethod
