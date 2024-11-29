841af3b3d825544aa377f295b586f08f051fbdc4
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-11-29 00:24:04 -0300

    excluir OK

--- site_cc/tests.py
+++ site_cc/tests.py
@@ -660,7 +660,7 @@ class ExcluirCulturaTest(LiveServerTestCase):
 
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fc-daygrid-event")))
         cultura_excluida = driver.find_element(By.CSS_SELECTOR, ".fc-daygrid-event")
-        cultura_excluida.click()
+        driver.execute_script("arguments[0].click();", cultura_excluida)
 
         time.sleep(3)
 
